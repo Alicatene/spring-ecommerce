59744c9715054beb787d92041d5687fb2799bc35
alicatene <minguez2794@gmail.com>
2024-12-31 11:47:50 +0100

    creado el repository y las clases de servicio y el método de show controller

--- src/main/java/com/curso/ecomerce/model/Usuario.java
+++ src/main/java/com/curso/ecomerce/model/Usuario.java
@@ -1,10 +1,10 @@
 package com.curso.ecomerce.model;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
